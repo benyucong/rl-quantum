343e1af3b06b664de5e18fd4765e59e39b705eb7
benyucong <benyucong@gmail.com>
2025-06-17 10:44:58 +0300

    run on triton

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=gsm8k_multiturn
+#SBATCH --partition=gpu-h200-141g-short
+#SBATCH --cpus-per-task=48
+#SBATCH --time=10:00:00
+#SBATCH --mem=50G
+#SBATCH --gres=gpu:1
+
+export UV_CACHE_DIR=$$WRKDIR/.cache/uv
+
+# uv pip install -e verl
+# uv pip install vllm==0.8.4
+MAX_JOBS=4 uv pip install flash-attn==2.5.8 --no-build-isolation
+# uv pip install -e ".[acecoder,torl]"
+# uv pip install dill==0.4.0 fsspec==2025.3.2 protobuf==5.29.4
