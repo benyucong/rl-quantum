4937c77697e3785fb53a9f47972700665b1cd1d9
benyucong <benyucong@gmail.com>
2025-06-23 15:58:22 +0300

    fix verl version

--- examples/train/quantum/train_hpc.sh
+++ examples/train/quantum/train_hpc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=demo-verl-tool
-#SBATCH --time=04:00:00
+#SBATCH --job-name=quantum_3B
+#SBATCH --time=10:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=400GB
 #SBATCH --gpus=8
-#SBATCH --partition=gpu-h200-141g-short
+#SBATCH --partition=gpu-h200-141g-ellis
 
-
+cd ../../../
 bash examples/train/quantum/train_qwen_3B_quantum.sh
--- install_flash_attn.sh
+++ install_flash_attn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=gsm8k_multiturn
+#SBATCH --job-name=install_flash_attn
 #SBATCH --partition=gpu-h200-141g-short
 #SBATCH --cpus-per-task=48
 #SBATCH --time=10:00:00
@@ -8,6 +8,11 @@
 
 export UV_CACHE_DIR=$$WRKDIR/.cache/uv
 
+module load gcc cuda cmake openmpi
+module load scicomp-python-env/2024-01
+module load scicomp-llm-env
+
+source .venv/bin/activate
 # uv pip install -e verl
 # uv pip install vllm==0.8.4
 MAX_JOBS=4 uv pip install flash-attn==2.5.8 --no-build-isolation
